17fdd0dc2fb3a5609e684fcccaca031f402d003f
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2020-09-15 01:06:01 +0200

    ported to updated trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
 //use error_level::ErrorLevel;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn;
+use syn::{self, punctuated::Punctuated, Variant, token::Comma, Attribute};
 
 #[proc_macro_derive(ErrorLevel, attributes(level))]
 pub fn log_level_derive(input: TokenStream) -> TokenStream {
     // Construct a representation of Rust code as a syntax tree
     // that we can manipulate
     let ast = syn::parse(input).unwrap();
-    dbg!(&ast);
 
     // Build the trait implementation
     impl_error_level_macro(&ast)
 }
 
+#[derive(Debug)]
 enum Level {
     No,
     Trace,
@@ -23,27 +23,85 @@ enum Level {
     Error,
 }
 
-struct Attr {
+impl Level {
+    fn from_ident(id: &syn::Ident) -> Self {
+        match id.to_string().as_str() {
+            "No" => Self::No,
+            "Trace" => Self::Trace,
+            "Debug" => Self::Debug,
+            "Info" => Self::Info,
+            "Warn" => Self::Warn,
+            "Error" => Self::Error,
+            _ => panic!("options are only: No, Trace, Debug, Info, Warn or Error"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Marked {
     level: Level,
-    ident: syn::Ident,
+    variant_id: syn::Ident,
+}
+
+fn log_level(v: &Variant) -> Option<Level> { 
+    fn has_level_path(m: &syn::MetaList) -> bool {
+        if let Some(ident) = m.path.get_ident() {
+            ident == "level"
+        } else {
+            false
+        }
+    }
+    fn unwrap_meta(n: &syn::NestedMeta) -> &syn::Meta {
+        if let syn::NestedMeta::Meta(m) = n {
+            return m;
+        }
+        panic!("nested argument list should not be a rust literal but a structured meta item");
+    }
+
+    for a in &v.attrs {
+        let m = a.parse_meta().unwrap();
+        if let syn::Meta::List(list) = m { 
+            if !has_level_path(&list){continue;}
+            let nested = list.nested.first().unwrap();
+            let meta = unwrap_meta(&nested);
+            let ident = meta.path().get_ident().unwrap();
+            return Some(Level::from_ident(ident));
+        }
+    }
+    None
+}
+
+fn marked_variants(variants: &Punctuated<Variant, Comma>) -> Vec<Marked> {
+    let mut marked = Vec::new();
+    for v in variants { 
+        if let Some(level) = log_level(v){
+            let variant_id = v.ident.clone();
+            marked.push(Marked {
+                level,
+                variant_id
+            });
+        }
+    }
+    marked
 }
 
 fn impl_error_level_macro(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
     let data = &ast.data;
     let variants = &unwrap_enum(data).variants;
-     
     //save list of variants with a level attribute
+    let marked = marked_variants(variants);
+    
     //for idents without attr call the error_level function
     //if error_level is undefined for that type the user will
     //get an error
 
     let gen = quote! {
         impl ErrorLevel for #name {
-            fn error_level(&self) -> log::Level {
+            fn error_level(&self) -> Option<log::Level> {
                 //for each attr add a case that makes the report
                 println!("Hello, Macro! My name is {}!", stringify!(#name));
-                log::Level::Warn
+                Some(log::Level::Warn)
             }
         }
     };
--- tests/simple.rs
+++ tests/simple.rs
@@ -1,28 +1,32 @@
 use error_level::ErrorLevel;
 use error_level_derive::ErrorLevel;
 use log::Level;
+use simplelog::{LevelFilter, Config, SimpleLogger};
+
+#[allow(dead_code)]
+#[derive(Debug)]
+#[derive(ErrorLevel)]
+pub enum OuterError {
+    #[level(Info)]
+    Error0,
+    Error1,
+}
+
+#[derive(Debug)]
+#[derive(ErrorLevel)]
+pub enum CustomError {
+    #[level(Warn)]
+    ErrorA,
+    #[level(Info)]
+    ErrorB,
+    #[level(No)]
+    ErrorC,
+    ErrorD(OuterError),
+}
 
 #[test]
 fn it_works() {
-
-    #[allow(dead_code)]
-    #[derive(ErrorLevel)]
-    pub enum OuterError {
-        #[level(Info)]
-        Error0,
-        Error1,
-    }
-
-    #[derive(ErrorLevel)]
-    pub enum CustomError {
-        #[level(Warn)]
-        ErrorA,
-        #[level(Info)]
-        ErrorB,
-        #[level(No)]
-        ErrorC,
-        ErrorD(OuterError),
-    };
+    SimpleLogger::init(LevelFilter::Trace, Config::default()).unwrap();
 
     let a = CustomError::ErrorA;
     let b = CustomError::ErrorB;
@@ -32,6 +36,7 @@ fn it_works() {
     //TODO move to directly reporting instead of returning a level
     //can implement both as separate Traits, one returning Option<Level>
     //and one using that to actually report
-    assert_eq!(a.error_level(), Level::Warn);
-    assert_eq!(b.error_level(), Level::Warn);
+    assert_eq!(a.error_level(), Some(Level::Warn));
+    assert_eq!(b.error_level(), Some(Level::Warn));
+    b.log_error();
 }
