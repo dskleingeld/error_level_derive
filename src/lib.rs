9c364c1508f1dcaed7bd39a087f8b8fa3c9c9b48
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2020-09-17 01:57:24 +0200

    adds hygiene (proper error reporting)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //use error_level::ErrorLevel;
 use proc_macro::TokenStream;
-use quote::{quote, TokenStreamExt, ToTokens};
-use syn::{self, punctuated::Punctuated, Variant, token::Comma, Attribute, Fields};
+use quote::{quote, quote_spanned, ToTokens};
+use syn::{self, spanned::Spanned, punctuated::Punctuated, Variant, token::Comma, Attribute, Fields};
 
 #[proc_macro_derive(ErrorLevel, attributes(level))]
 pub fn log_level_derive(input: TokenStream) -> TokenStream {
@@ -65,7 +65,7 @@ fn has_level_path(m: &syn::MetaList) -> bool {
     }
 }
 
-fn log_level(v: &Variant) -> Option<Level> { 
+fn with_log_level(v: &Variant) -> Option<Level> { 
     fn unwrap_meta(n: &syn::NestedMeta) -> &syn::Meta {
         if let syn::NestedMeta::Meta(m) = n {
             return m;
@@ -89,7 +89,7 @@ fn log_level(v: &Variant) -> Option<Level> {
 fn marked_variants(variants: &Punctuated<Variant, Comma>) -> Vec<Marked> {
     let mut marked = Vec::new();
     for v in variants { 
-        if let Some(level) = log_level(v){
+        if let Some(level) = with_log_level(v){
             let variant_id = v.ident.clone();
             marked.push(Marked {
                 level,
@@ -106,15 +106,25 @@ struct WithInnError {
     variant_id: syn::Ident,
 }
 
-fn inner_err(v: &Variant) -> Option<syn::Ident> { 
-    // fn unwrap_meta(n: &syn::NestedMeta) -> &syn::Meta {
-    //     if let syn::NestedMeta::Meta(m) = n {
-    //         return m;
-    //     }
-    //     panic!("nested argument list should not be a rust literal but a structured meta item");
-    // }
+enum Valid {
+    Yes(syn::Ident),
+    No(proc_macro2::Span),
+}
 
-    //filter out anything with a level attribute
+fn is_valid_inner(ty: &syn::Type) -> Valid {
+    match ty {
+        syn::Type::Path(p) => Valid::Yes(p.path.get_ident().unwrap().clone()),
+        syn::Type::Reference(r) =>
+            if let syn::Type::Path(p) = &*r.elem {
+                Valid::Yes(p.path.get_ident().unwrap().clone())
+            } else {
+                Valid::No(r.span())
+            },
+        _ => Valid::No(ty.span())
+    }
+}
+
+fn has_inner(v: &Variant) -> Option<&syn::Type> { 
     for a in &v.attrs {
         let m = a.parse_meta().unwrap();
         if let syn::Meta::List(list) = m { 
@@ -124,51 +134,86 @@ fn inner_err(v: &Variant) -> Option<syn::Ident> {
 
     if let Fields::Unnamed(syn::FieldsUnnamed {ref unnamed, ..}) = v.fields {
         let ty = &unnamed.first()?.ty;
-        if let syn::Type::Path(syn::TypePath {ref path, ..}) = ty {
-            let ident = path.get_ident()?;
-            return Some(ident.clone());
-        }
+        Some(ty)
+    } else {
+        None
     }
-    None
 }
 
-fn with_inner_error(variants: &Punctuated<Variant, Comma>) -> Vec<WithInnError> {
-    let mut w_inn_err = Vec::new();
-    for v in variants { 
-        if let Some(inner_id) = inner_err(v){
+// fn with_inner_error(variants: &Punctuated<Variant, Comma>) -> Vec<WithInnError> {
+//     let mut w_inn_err = Vec::new();
+//     for v in variants { 
+//         if let Some(inner_id) = could_have_inner_err(v){
+//             let variant_id = v.ident.clone();
+//             w_inn_err.push(WithInnError {
+//                 inner_id,
+//                 variant_id
+//             });
+//         }
+//     }
+//     w_inn_err
+// }
+
+fn extract_variants(variants: &Punctuated<Variant, Comma>) -> (Vec<Marked>, Vec<WithInnError>, Vec<proc_macro2::TokenStream>) {
+    let mut marked = Vec::new();
+    let mut w_inner = Vec::new();
+    let mut errs = Vec::new();
+
+    for v in variants {
+        if let Some(level) = with_log_level(v){
             let variant_id = v.ident.clone();
-            w_inn_err.push(WithInnError {
-                inner_id,
+            marked.push(Marked {
+                level,
                 variant_id
             });
+        } else if let Some(inner) = has_inner(v){
+            match is_valid_inner(inner) {
+                Valid::Yes(inner_id) => {    
+                    let variant_id = v.ident.clone();
+                    w_inner.push(WithInnError {
+                        inner_id,
+                        variant_id
+                    });
+                },
+                Valid::No(span) => {
+                    errs.push(quote_spanned! {
+                        span =>
+                        compile_error!("Need 'Level' attribute, variant content can not get an 'ErrorLevel' trait implementation");
+                    });
+                },
+            }
+        } else {
+            errs.push(quote_spanned! {
+                v.span() =>
+                compile_error!("Need 'Level' attribute");
+            })
         }
     }
-    dbg!(&w_inn_err);
-    w_inn_err
+    (marked, w_inner, errs)
 }
 
 fn impl_error_level_macro(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
     let data = &ast.data;
     let variants = &unwrap_enum(data).variants;
+    let (marked, w_inner, errs) = extract_variants(variants);
+
     //save list of variants with a level attribute
-    let marked = marked_variants(variants);
     let level_with_attr = marked.iter().map(|m| &m.level);
     let ident_with_attr = marked.iter().map(|m| &m.variant_id);
 
     //for idents without attr call the error_level function
     //if error_level is undefined for that type the user will
-    let w_inner_err = with_inner_error(variants);
-    //let ident_w_inner_err = unimplemented!();
+    let ident_no_attr = w_inner.iter().map(|m| &m.variant_id);
 
     let gen = quote! {
         impl ErrorLevel for #name {
             fn error_level(&self) -> Option<log::Level> {
                 match self {
                     #(#name::#ident_with_attr => #level_with_attr,)*
-                    //#(#name::#ident_w_inner_err(inn_err) => inn_err.error_level(),)*
-                    _ => todo!(),
+                    #(#name::#ident_no_attr(inn_err) => inn_err.error_level(),)*
                 }
+                #(#errs)*;
             }
         }
     };
